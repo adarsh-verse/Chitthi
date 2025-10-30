4144e43f934efe3e7b485a0146761b5217b36ae0
Adarsh <adarshkumarmishra712@gmail.com>
2025-10-30 18:39:17 +0530

    updated backend url

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {  store } from './redux/store.js'
 // import { PersistGate } from 'redux-persist/integration/react'
 // import { persistStore } from 'redux-persist'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://chitthi-goje.onrender.com/"
 
 // const persistor = persistStore(store)
 createRoot(document.getElementById('root')).render(
