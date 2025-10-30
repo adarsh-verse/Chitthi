7a9fc29dfb2d628de6a559508ac0c5ad79ccb62a
Adarsh <adarshkumarmishra712@gmail.com>
2025-10-30 21:46:10 +0530

    please get fixed

--- index.js
+++ index.js
@@ -17,16 +17,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json()); 
