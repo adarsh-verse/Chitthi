1f371e702c9c3087c28061a16ee20def0ed47c68
Adarsh <adarshkumarmishra712@gmail.com>
2025-10-30 18:44:32 +0530

    fixed url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const port = process.env.PORT || 5000
 const app = express()
 app.use(cors({
-    origin:"https://adarsh-verse.github.io/Chitthi/",
+    origin:"https://adarsh-verse.github.io",
     credentials:true
 }))
 app.use(express.json()); 
